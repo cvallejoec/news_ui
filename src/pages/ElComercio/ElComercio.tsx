4dc6459fd45bb23228151c07c155d368808957ee
Carlos Vallejo <carlos.vallejo@101grados.com>
2021-06-23 22:07:44 -0500

    Tabs implemented

--- src/pages/ElComercio/ElComercio.tsx
+++ src/pages/ElComercio/ElComercio.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { config } from '../../config';
 import { useSpider } from '../../hooks/useSpider';
+import { TabsC, TabProps } from '../../components/Tabs/Tabs';
 import { Articles } from '../../components/Articles/Articles';
 import { Loading } from '../../components/Loading/Loading';
 
@@ -9,11 +10,25 @@ export const ElComercio = () => {
   const { spiders } = config;
   const { data, loading } = useSpider(spiders.el_comercio);
 
-  return (
-    <div>
-      {/* <h1>El Comercio</h1> */}
-      {loading && <Loading />}
-      {data.length > 0 && <Articles news={data} />}
-    </div>
-  );
+  const tabs: TabProps[] = [
+    {
+      label: 'Última Hora',
+      children: (
+        <>
+          {loading && <Loading />}
+          {data.length > 0 && <Articles news={data} />}
+        </>
+      ),
+    },
+    {
+      label: 'Política',
+      children: <h3>Política</h3>,
+    },
+    {
+      label: 'Economía',
+      children: <h3>Economía</h3>,
+    },
+  ];
+
+  return <TabsC tabs={tabs} />;
 };
